99f625fe1afba4c76ff1a03fee49ce42ca66f012
Rubén Vidal <ruben.vidal.castellet@gmail.com>
2011-04-18 15:37:39 +0200

    eleminar los ficheros swap

--- app/models/encuesta.rb
+++ app/models/encuesta.rb
@@ -1,4 +1,5 @@
 class Encuesta < ActiveRecord::Base
+  attr_accessible :nombre, :preguntas_attributes
   has_many :preguntas, :dependent => :destroy
-  accepts_nested_attributes_for :preguntas
+  accepts_nested_attributes_for :preguntas, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
